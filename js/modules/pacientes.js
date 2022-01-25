51c0b6b9686e2845bb5138e484d7390c083cd88b
mehfius <matheus.ferraz@gmail.com>
2022-01-25 20:35:24 +0000

    modules > item > paciente codigo

--- js/modules/pacientes.js
+++ js/modules/pacientes.js
@@ -5,11 +5,13 @@ function loadPacientes(element,array){
       var ePaciente   = createObject('{"tag":"pacientes"}');
       var eDiv        = createObject('{"tag":"div"}');
       var eIcon       = createObject('{"tag":"icon","class":"icon-user"}');
-
+     // var eId         = createObject('{"tag":"id"}');
 
       eDiv.append(createObject('{"tag":"label","innerhtml":"'+array.label+'"}'));
+      eDiv.append(createObject('{"tag":"id","innerhtml":"'+array.id+'"}'));
 
-      ePaciente.appendChild(eDiv);
+
+      ePaciente.append(eDiv);
       element.appendChild(ePaciente);
      
    }
