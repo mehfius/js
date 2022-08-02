de87e0ed4605a20239c0930e74b4e4523186efd9
Matheus Ferraz <matheus.ferraz@gmail.com>
2022-08-02 20:15:30 +0000

    function insertAnexos

--- export/all.js
+++ export/all.js
@@ -5828,11 +5828,13 @@ function insertAnexos(anexos, filename) {
   const config = JSON.parse(localStorage.config);
   const user = JSON.parse(localStorage.user);
 
+  const supabasekey = localStorage.supabasekey;
+
 
   (async () => {
     const rawResponse = await fetch(config.insertfile, {
       method: 'POST',
-      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', 'apikey': supabasekey },
       body: JSON.stringify({ euuid: user.session, eanexos: anexos, efilename: filename })
     });
 
